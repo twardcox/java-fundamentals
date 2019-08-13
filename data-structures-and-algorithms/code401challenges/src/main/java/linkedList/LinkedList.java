3e62f8aee0715a04206f7f697e1d71a5b5a0efe5
twardcox <t.ward.cox@gmail.com>
2019-08-13 09:25:46 -0700

    Challenge complete

--- data-structures-and-algorithms/code401challenges/src/main/java/linkedList/LinkedList.java
+++ data-structures-and-algorithms/code401challenges/src/main/java/linkedList/LinkedList.java
@@ -1,22 +1,10 @@
 package linkedList;
 
-public class LinkedList {
-  public Node head;
+public class LinkedList<T> {
+  public Node<T> head;
 
-  static class Node {
-    int value;
-    Node next;
-
-//    Constructor
-    Node(int v)
-    {
-      value = v;
-      next = null;
-    }
-  }
-
-  public LinkedList insert(LinkedList list, int value){
-    Node new_node = new Node(value);
+  public LinkedList insert(LinkedList list, T value){
+    Node<T> new_node = new Node<T>(value);
     new_node.next = null;
 
     if (list.head == null) {
@@ -31,8 +19,8 @@ public class LinkedList {
     return list;
   }
 
-  public boolean includes(LinkedList list, int value){
-    Node currentNode = list.head;
+  public boolean includes(LinkedList<Integer> list, T value){
+    Node<Integer> currentNode = list.head;
     while (currentNode != null){
       if (currentNode.value == value){
         return true;
@@ -42,8 +30,8 @@ public class LinkedList {
     return false;
   }
 
-  public String toString(LinkedList list){
-    Node currentNode = list.head;
+  public String toString(LinkedList<Integer> list){
+    Node<Integer> currentNode = list.head;
 
     StringBuilder out = new StringBuilder();
 
@@ -58,6 +46,50 @@ public class LinkedList {
     return out.toString();
   }
 
+  public void append(LinkedList<T> list, T value){
+    Node<T> currentNode = list.head;
+
+    while (currentNode.next != null){
+      currentNode = currentNode.next;
+    }
+    Node<T> nextNode = new Node<>(value);
+    currentNode.next = nextNode;
+  }
+
+  public void insertAfter(LinkedList<T> list, T value, T newValue){
+    Node<T> currentNode = list.head;
+
+    while (currentNode.next != null){
+      currentNode = currentNode.next;
+      if (currentNode.value == value){
+        break;
+      }
+    }
+    T temp = (T) currentNode.next;
+    Node<T> newNext = new Node<>(newValue);
+    newNext.next = (Node<T>) temp;
+    currentNode.next = newNext;
+  }
+
+  public void insertBefore(LinkedList<T> list, T value, T newValue){
+    Node currentNode = list.head;
+
+    while (currentNode.next != null){
+
+      if (currentNode.next.value == value){
+        break;
+      }
+      currentNode = currentNode.next;
+    }
+    Node temp = currentNode.next;
+    Node newNext = new Node<>(newValue);
+    newNext.next = temp;
+    currentNode.next = newNext;
+  }
+
+
 }
+
+
 
 
--- data-structures-and-algorithms/code401challenges/src/test/java/linkedList/LinkedListTest.java
+++ data-structures-and-algorithms/code401challenges/src/test/java/linkedList/LinkedListTest.java
@@ -15,42 +15,73 @@ public class LinkedListTest {
 
   @Test
   public void testFirstNode(){
-    LinkedList list = new LinkedList();
+    LinkedList<Integer> list = new LinkedList<Integer>();
     list.insert(list, 1);
     list.insert(list, 2);
     list.insert(list, 3);
-    assertEquals("testFirstNode head value should be 1.", list.head.value, 1);
+    assertEquals("testFirstNode head value should be 1.", "1, 2, 3", list.toString(list));
   }
 
 
   @Test
   public void testInsertNewNode() {
-    LinkedList list = new LinkedList();
+    LinkedList<Integer> list = new LinkedList<Integer>();
     list.insert(list, 1);
     assertNotNull("testInsertNewNode head value should not be null.", list);
   }
 
   @Test
   public void testIncludesTrue() {
-    LinkedList list = new LinkedList();
+    LinkedList<Integer> list = new LinkedList<Integer>();
     list.insert(list, 1);
     assertTrue("testIncludesTrue value should be True.", list.includes(list, 1));
   }
 
   @Test
   public void testIncludesFalse() {
-    LinkedList list = new LinkedList();
+    LinkedList<Integer> list = new LinkedList<Integer>();
     list.insert(list, 1);
     assertFalse("testIncludesFalse value should be False.", list.includes(list, 2));
   }
 
   @Test
   public void testInsertMultiple(){
-    LinkedList list = new LinkedList();
+    LinkedList<Integer> list = new LinkedList<Integer>();
     list.insert(list, 1);
     list.insert(list, 2);
     list.insert(list, 3);
-    assertEquals("testInsertMultiple value should be Equal.", list.toString(list), "1, 2, 3");
+    assertEquals("testInsertMultiple value should be Equal.", "1, 2, 3", list.toString(list));
+  }
+
+  @Test
+  public void testAppend(){
+    LinkedList<Integer> list = new LinkedList<>();
+    list.insert(list, 1);
+    list.insert(list, 2);
+    list.append(list, 3);
+    assertEquals("testAppend value should be Equal.", "1, 2, 3", list.toString(list));
+  }
+
+  @Test
+  public void testInsertAfter(){
+    LinkedList<Integer> list = new LinkedList<Integer>();
+    list.insert(list, 1);
+    list.insert(list, 2);
+    list.insert(list, 3);
+    list.insert(list, 5);
+    list.insertAfter(list, 3, 4);
+    assertEquals("testInsertAfter value should be Equal.", "1, 2, 3, 4, 5", list.toString(list));
+  }
+
+  @Test
+  public void testInsertBefore(){
+    LinkedList<Integer> list = new LinkedList<Integer>();
+    list.insert(list, 1);
+    list.insert(list, 2);
+    list.insert(list, 3);
+    list.insert(list, 5);
+    list.insertBefore(list, 5, 4);
+    assertEquals("testInsertAfter value should be Equal.", "1, 2, 3, 4, 5",list.toString(list));
   }
 
 }
