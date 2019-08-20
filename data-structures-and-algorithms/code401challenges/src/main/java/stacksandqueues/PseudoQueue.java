3533e9356faba92bf6614fddebb7ac2b826c0f25
twardcox <t.ward.cox@gmail.com>
2019-08-20 09:25:26 -0700

    PseudoQueue challenge complete

---
+++ data-structures-and-algorithms/code401challenges/src/main/java/stacksandqueues/PseudoQueue.java
@@ -0,0 +1,35 @@
+package stacksandqueues;
+
+public class PseudoQueue<T> {
+  protected Stack<T> front = new Stack<>();
+  protected Stack<T> back = new Stack<>();
+
+  public void enqueue(T value) {
+
+    if (front.top == null) {
+      back.push(value);
+    } else {
+      while (front.top != null) {
+        back.push(front.pop());
+      }
+      back.push(value);
+    }
+
+  }
+
+  public T dequeue(){
+    if (front.top == null && back.top == null){
+      return null;
+    }
+
+    if(back.top == null){
+      return front.pop();
+    } else {
+      while (back.top != null){
+        front.push(back.pop());
+      }
+      return front.pop();
+    }
+  }
+
+}
